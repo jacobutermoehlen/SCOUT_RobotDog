cec5e7cd8d71c7789405dd5d699ba3f3a6a36625
Jacob Utermoehlen <jacobutermoehlen@outlook.com>
2025-01-26 15:24:31 +0100

    32th Commit

--- HERO_GUI/HERO_GUI/MainWindow.xaml.cs
+++ HERO_GUI/HERO_GUI/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -23,6 +24,12 @@ namespace HERO_GUI
         int sensorTCPPort = 67890;
         string jetsonTCPIp = "192.168.4.1";
         int jetsonTCPPort = 12345;
+        private Thread jetsonTcpThread;
+        private TcpClient tcpClient;
+        private NetworkStream networkStream;
+        private bool jetsonTcpIsRunning = true;
+        private ManualResetEvent messageReceivedEvent = new ManualResetEvent(false);
+        private string receivedMessage = string.Empty;
 
         //variables
         int velocity = 50;
@@ -34,14 +41,76 @@ namespace HERO_GUI
 
 
             //Starting Threads
-            Thread tcpSensor_readThread = new Thread(() => tcpSensor_ReadContin(sensorTCPIp, sensorTCPPort));
+            //Thread tcpSensor_readThread = new Thread(() => tcpSensor_ReadContin(sensorTCPIp, sensorTCPPort));
+            //Start the Jetson TCP Control Thread
+            jetsonTcpThread = new Thread(InitializeTcpConnection);
+            jetsonTcpThread.IsBackground = true;
+            jetsonTcpThread.Start();
+        }
 
+        private void InitializeTcpConnection()
+        {
+            try
+            {
+                tcpClient = new TcpClient(jetsonTCPIp, jetsonTCPPort);
+                networkStream = tcpClient.GetStream();
+
+                while (jetsonTcpIsRunning)
+                {
+                    if (networkStream.DataAvailable)
+                    {
+                        byte[] buffer = new byte[1024];
+                        int bytesRead = networkStream.Read(buffer, 0, buffer.Length);
+                        receivedMessage = Encoding.UTF8.GetString(buffer, 0, bytesRead).Trim();
+
+                        // Check if we have a waiting thread
+                        if (!string.IsNullOrEmpty(receivedMessage))
+                        {
+                            messageReceivedEvent.Set(); // Unblock waiting thread
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        private void SendMessage(string message)
+        {
+            if(networkStream != null && networkStream.CanWrite)
+            {
+                byte[] data = Encoding.UTF8.GetBytes(message);
+                networkStream.Write(data, 0, data.Length);
+            }
+        }
+
+        private void SendAndWaitForResponse(string messageToSend, string expectedResponse, string answer)
+        {
+            new Thread(() =>
+            {
+                SendMessage(messageToSend);
+                messageReceivedEvent.Reset(); // Prepare to wait
+
+                // Wait until the expected response is received
+                bool received = messageReceivedEvent.WaitOne(TimeSpan.FromSeconds(10)); // Wait up to 10 seconds
+                if (received && receivedMessage == expectedResponse)
+                {
+                    SendMessage(answer);
+                }
+                else
+                {
+
+                }
+            }).Start();
         }
 
         //movement control methods
         private void move_forward_btn_Click(object sender, RoutedEventArgs e)
         {
-
+            int veloc = Convert.ToInt16(velocity_sld.Value);
+            new Thread(() => SendAndWaitForResponse("changeAngle0", "changedAngle",$"moveForward{veloc}")).Start();
         }
         private void velocity_sld_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
@@ -73,6 +142,21 @@ namespace HERO_GUI
         private void camStopRec_btn_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
 
+        }
+
+        private void move_stop_btn_Click(object sender, RoutedEventArgs e)
+        {
+            new Thread(() => SendMessage("moveN230")).Start();
+        }
+
+        private void move_left_btn_Click(object sender, RoutedEventArgs e)
+        {
+            new Thread(() => SendAndWaitForResponse("changeAngle-30", "changedAngle", "moveForward5")).Start();
+        }
+
+        private void move_right_btn_Click(object sender, RoutedEventArgs e)
+        {
+            new Thread(() => SendAndWaitForResponse("changeAngle30", "changedAngle", "moveForward5")).Start();
         }
     }
 }
